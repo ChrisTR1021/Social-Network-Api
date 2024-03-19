242024f22a1cedc4edb735f79656f003801a909a
ChrisTR1021 <christianridgell1021@gmail.com>
2024-03-18 23:35:33 -0400

    added function that grabs friend count and length of array

--- models/User.js
+++ models/User.js
@@ -1,22 +1,50 @@
-const {Schema, model} = require("mongoose");
+const { Schema, model, Types } = require('mongoose');
+const thoughtSchema = require('./Thought');
 
-const userSchema = new Schema({
+//  Used for user model creation
+const userSchema = new Schema(
+  {
     username: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
     },
     email: {
-        type: String,
-        unique: true,
-        required: true,
-        match: [/.+@.+\..+/, "You entered a invalid email, please enter another",]
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "You entered a invalid email, Please try again"],
     },
     thoughts: [
         {
             type: Schema.Types.ObjectId,
-            ref: "Thought"
+            ref: 'thought'
         },
     ],
-})
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+
+
+// Creates `friendCount` that retrieves 'friends' array 
+
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
